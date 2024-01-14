d17bbb51b259cee25e089446cbace7495482ae7e
Karlo Hajdinjak <khajdinja20@foi.hr>
2024-01-14 15:32:33 +0100

    Inital home screen and login screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Auth from "./Auth"
+import Home from "./Home";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ client/src/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Screen!</h1>
+      <p>This is a simple home screen.</p>
+    </div>
+  );
+};
+
+export default Home;
